7b00c26383a4d060dc1f3b3eab297fc240922052
htferretti <htferretti@gmail.com>
2025-02-06 15:52:48 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import EstiloGlobal from './styles.ts'
+
+import Teste from './Teste.tsx'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  );
+}
+
+export default App;
